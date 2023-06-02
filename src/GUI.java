fb27c75fdd651b6d9fffc1193dc47d5d0e543e7f
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2023-06-02 01:40:59 -0400

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -3,18 +3,22 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI extends JFrame {
+public class GUI extends JFrame 
+{
     private JButton[][] buttons;
     private Logic game;
 
-    public GUI() {
+    public GUI() 
+    {
         game = new Logic();
         buttons = new JButton[3][3];
 
         // Set up the GUI
         JPanel panel = new JPanel(new GridLayout(3, 3));
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
+        for (int row = 0; row < 3; row++) 
+        {
+            for (int col = 0; col < 3; col++) 
+            {
                 buttons[row][col] = new JButton();
                 buttons[row][col].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 60));
                 buttons[row][col].addActionListener(new ButtonClickListener(row, col));
@@ -29,33 +33,57 @@ public class GUI extends JFrame {
         setVisible(true);
     }
 
-    private class ButtonClickListener implements ActionListener {
+    private class ButtonClickListener implements ActionListener 
+    {
         private int row;
         private int col;
 
-        public ButtonClickListener(int row, int col) {
+        public ButtonClickListener(int row, int col) 
+        {
             this.row = row;
             this.col = col;
         }
 
         @Override
-        public void actionPerformed(ActionEvent e) {
-            if (game.isGameOver() || game.getWinner() != Logic.EMPTY) {
+        public void actionPerformed(ActionEvent e) 
+        {
+            if (game.isGameOver() || game.getWinner() != Logic.EMPTY) 
+            {
                 return;
             }
 
-            if (game.makeMove(row, col)) {
+            if (game.makeMove(row, col)) 
+            {
                 buttons[row][col].setText(game.getCurrentPlayer() == Logic.X ? "X" : "O");
-                if (game.getWinner() != Logic.EMPTY) {
-                    JOptionPane.showMessageDialog(GUI.this, "Player " + game.getWinner() + " wins!");
-                } else if (game.isGameOver()) {
+                if (game.getWinner() != Logic.EMPTY) 
+                {
+                    String winner = game.getWinner() == Logic.X ? "Player X" : "Player O";
+                    JOptionPane.showMessageDialog(GUI.this, winner + " loses!");
+                    resetGame();
+                } 
+                else if (game.isGameOver()) 
+                {
                     JOptionPane.showMessageDialog(GUI.this, "It's a tie!");
+                    resetGame();
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
+    private void resetGame() 
+    {
+        game.clearBoard();
+        for (int row = 0; row < 3; row++) 
+        {
+            for (int col = 0; col < 3; col++) 
+            {
+                buttons[row][col].setText("");
+            }
+        }
+    }
+
+    public static void main(String[] args) 
+    {
         SwingUtilities.invokeLater(GUI::new);
     }
 }

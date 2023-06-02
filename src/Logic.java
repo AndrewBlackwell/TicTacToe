840e3e2825f1d5ca5b966c879ae83bbaf5dadf24
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2023-06-02 01:41:37 -0400

    Update Logic.java

--- src/Logic.java
+++ src/Logic.java
@@ -44,4 +44,76 @@ public class Logic
     {
         return winner;
     }
+
+    public void clearBoard() 
+    {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = EMPTY;
+            }
+        }
+        currentPlayer = X;
+        gameOver = false;
+        winner = EMPTY;
+    } 
+
+    private void checkGameOver(int lastRow, int lastCol) 
+    {
+        // Check row
+        if (board[lastRow][0] == board[lastRow][1] && board[lastRow][0] == board[lastRow][2]) 
+        {
+            gameOver = true;
+            winner = currentPlayer;
+            return;
+        }
+
+        // Check column
+        if (board[0][lastCol] == board[1][lastCol] && board[0][lastCol] == board[2][lastCol]) 
+        {
+            gameOver = true;
+            winner = currentPlayer;
+            return;
+        }
+
+        // Check diagonal
+        if (lastRow == lastCol) 
+        {
+            if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) 
+            {
+                gameOver = true;
+                winner = currentPlayer;
+                return;
+            }
+        }
+
+        // Check anti-diagonal
+        if (lastRow + lastCol == 2) 
+        {
+            if (board[0][2] == board[1][1] && board[0][2] == board[2][0]) 
+            {
+                gameOver = true;
+                winner = currentPlayer;
+                return;
+            }
+        }
+
+        // Check tie
+        boolean tie = true;
+        for (int row = 0; row < 3; row++) 
+        {
+            for (int col = 0; col < 3; col++) 
+            {
+                if (board[row][col] == EMPTY) 
+                {
+                    tie = false;
+                    break;
+                }
+            }
+        }
+        if (tie) 
+        {
+            gameOver = true;
+            winner = EMPTY;
+        }
+    }
 }
